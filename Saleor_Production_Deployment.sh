3b2524bec93e2e08d6b2c59588e6e194ecf92c88
Vague Rabbit <ethanvandal@gmail.com>
2021-01-24 01:22:10 -0800

    venv

--- Saleor_Production_Deployment.sh
+++ Saleor_Production_Deployment.sh
@@ -79,7 +79,7 @@ echo "Installing dependencies..."
 case "$OS" in
         Debian)
                 sudo apt-get update
-                sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-virtualenv
+                sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-venv
                 sudo python3 -m pip install --upgrade pip
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
                 sudo apt-get install -y nodejs npm postgresql postgresql-contrib
@@ -96,7 +96,7 @@ case "$OS" in
 
         Ubuntu)
                 sudo apt-get update
-                sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-virtualenv
+                sudo apt-get install -y build-essential python3-dev python3-pip python3-cffi python3-venv
                 sudo python3 -m pip install --upgrade pip
                 sudo apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
                 sudo apt-get install -y nodejs npm postgresql postgresql-contrib
